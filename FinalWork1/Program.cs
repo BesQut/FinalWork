4cbcf82198d9d02df11cd0a791af3a47517a0c82
Ivan <nindsia146@mail.ru>
2023-01-24 13:38:09 +0400

    Программа для решения задачи

--- FinalWork1/Program.cs
+++ FinalWork1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] Array(int n)
+{
+    string[] array = new string[n];
+    for(int i = 0; i < n; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+void ShowArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            Console.Write(array[i] + "\t");
+        } 
+    }
+}
+
+Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] res = Array(n);
+ShowArray(res);
